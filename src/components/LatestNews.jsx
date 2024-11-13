359fc11086c4a2a0ef298f7d4d5af56758bf5d20
Your Name <khalidsaifullaha3@gmail.com>
2024-11-13 20:49:31 +0600

    added latest marque

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,5 +1,26 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
 const LatestNews = () => {
-  return <div></div>;
+  return (
+    <div className="flex gap-2 items-center bg-base-200 p-2">
+      <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
+      <Marquee pauseOnHover={true} speed={100} className="space-x-10">
+        <Link to="/news">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
+          deleniti.
+        </Link>
+        <Link to="/news">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
+          deleniti.
+        </Link>
+        <Link to="/news">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint,
+          quaerat.
+        </Link>
+      </Marquee>
+    </div>
+  );
 };
 
 export default LatestNews;
